177c5c8f13461a2f83dd3935388361523e06718c
Devarshi Boghani <boghanidevarshi@gmail.com>
2026-04-02 11:23:41 +0530

    Update file

--- Book-store_Project/config/dbConnection.js
+++ Book-store_Project/config/dbConnection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 // DB Connection
 const dbConnect = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/book")
+    // mongoose.connect("mongodb://127.0.0.1:27017/book")
+    mongoose.connect("mongodb+srv://boghanidevarshi_db_user:DevarshiBoghani@cluster0.kq8nhm0.mongodb.net/book")
     .then(() => console.log("DB Connect"))
     .catch(err => console.log(err));
 }
